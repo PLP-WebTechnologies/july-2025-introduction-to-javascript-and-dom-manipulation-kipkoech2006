06fe0d92bb833f2d14049e5b1aad98a2aa6d8d31
Ryan-Dev <ryankipkoechrotich9@gmail.com>
2025-09-03 00:41:47 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function multiply(numberone,numbertwo,numberthree)
+{return numberone*numbertwo*numberthree;}
+console.log(multiply(50,6,10));
+function changetext(){
+  let title=document.getElementById("title");
+  title.textcontent = "Hello web module"
+  title.style.color = "blue";
